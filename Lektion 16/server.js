a8ff884aba9a51f2f8c68d382f4d4f1f6e4feb31
Mikkel Cardon <91466197+MikkelCardon@users.noreply.github.com>
2025-11-10 09:36:42 +0100

    session example

--- Lektion 16/server.js
+++ Lektion 16/server.js
@@ -1,16 +1,23 @@
 import express from 'express'
 import session from 'express-session'
+import morgan from 'morgan'
+import path from "path"
+import { fileURLToPath } from 'url'
 
 const app = express()
 const PORT = 8080
 
 app.use(express.json())
+app.use(morgan("tiny"))
 app.use(session({
     secret: 'secret-key',
     resave: false,
     saveUninitialized: false,
 
 }))
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.get("/", (req, res) => {
     if(!req.session.viewCount){
@@ -21,6 +28,33 @@ app.get("/", (req, res) => {
     
     res.send(`View count: ${req.session.viewCount}`)
 })
+
+app.use(express.static('static'))
+
+app.get("/home", (req, res) => {
+    res.sendFile(path.join(__dirname, "static", "index.html"))
+})
+
+app.post("/login", async (req, res) => {
+    console.log(req.body);
+    
+    const {navn, password} = req.body
+    if(password === 'kode123' && navn){
+        req.session.navn = navn
+        res.send({ok: true})
+    }else {
+        res.send({ok: false})
+    }
+})
+
+app.get('/session', async (req, res) => {
+    const navn = req.session.navn;
+    if (navn) {
+        res.sendFile(path.join(__dirname, "static", "main.html"))
+    } else {
+        res.send("INGEN ADGANG")
+    }
+});
 
 app.listen(PORT, () =>{
     console.log("SERVER IS RUNNING ON PORT ", PORT)
---
+++ Lektion 16/static/script.js
@@ -0,0 +1,32 @@
+const navn = document.querySelector('#navn');
+const password = document.querySelector('#password');
+const login = document.querySelector('#login');
+const fejl = document.querySelector('#fejl');
+
+async function POST(url, data){
+    console.log(data);
+    
+
+    return await fetch(url, {
+        method: "POST",
+        headers: { "Content-type": "application/json"},
+        body: JSON.stringify(data)
+    })
+}
+
+
+login.onclick = async () => {
+try {
+const svar = await POST("/login",
+{navn: navn.value, password: password.value});
+if (svar.ok) {
+window.location.href = "/session";
+}
+else {
+password.value = "";
+fejl.innerHTML = "Forkert password!";
+}
+} catch (e) {
+fejl.innerHTML = e.name + ": " + e.message;
+}
+};
